352eba3690078f75b9c858dda9822c990d71f6b5
angie pinilla <angiepinillanova@gmail.com>
2025-04-15 16:50:11 -0500

    fix: remove unnecessary blank lines in environment configuration

--- src/config/environment/index.ts
+++ src/config/environment/index.ts
@@ -4,9 +4,7 @@ const AUTH_REDIRECT_URI = import.meta.env.VITE_AUTH0_REDIRECT_URI as string;
 const maxRetriesServices = 10;
 const maxRetriesPost = 1;
 const maxRetriesDelete = 1;
-
 const fetchTimeoutServices = 6000;
-
 const mediaQueryMobile = "(max-width: 770px)";
 
 const enviroment = {
